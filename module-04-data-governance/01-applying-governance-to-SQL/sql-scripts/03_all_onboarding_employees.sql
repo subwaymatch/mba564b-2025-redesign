f6e868cb835c23cac01d36cc20eba4109dbcf7ea
Ye Joo Park <subwaymatch@gmail.com>
2025-07-11 16:36:24 +0000

    Increase the number of sample rows, add markdown for explanation

--- module-04-data-governance/01-applying-governance-to-SQL/sql-scripts/03_all_onboarding_employees.sql
+++ module-04-data-governance/01-applying-governance-to-SQL/sql-scripts/03_all_onboarding_employees.sql
@@ -1,5 +1,5 @@
-SELECT FullName, StartDate, DepartmentName, JobTitle, PayType, PayRate
+SELECT FullName, StartDate, DepartmentName, JobTitle, PayType, PayRate, Status
 FROM employees
 JOIN departments ON employees.DepartmentID = departments.DepartmentID
 JOIN job_titles ON employees.JobTitleID = job_titles.JobTitleID
-WHERE Status = 'Active';
+WHERE Status = 'On-boarding';
--- module-04-data-governance/01-applying-governance-to-SQL/sql-scripts/06_employees_hired_after_2025.sql
+++ module-04-data-governance/01-applying-governance-to-SQL/sql-scripts/06_employees_hired_after_2025.sql
@@ -1,5 +1,5 @@
 SELECT FullName, StartDate, DepartmentName
 FROM employees
 JOIN departments ON employees.DepartmentID = departments.DepartmentID
-WHERE StartDate >= '2023-01-01'
+WHERE StartDate >= '2025-01-01'
 ORDER BY StartDate DESC;
